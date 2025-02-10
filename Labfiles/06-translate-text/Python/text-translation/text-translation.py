84c8b30ece3f301b2fafb48da94913a542ca7240
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-02-10 17:02:45 +0530

    Completed text translation

--- Labfiles/06-translate-text/Python/text-translation/text-translation.py
+++ Labfiles/06-translate-text/Python/text-translation/text-translation.py
@@ -2,6 +2,10 @@ from dotenv import load_dotenv
 import os
 import requests, json
 
+# import namespaces
+from azure.ai.translation.text import *
+from azure.ai.translation.text.models import InputTextItem
+
 def main():
     global translator_endpoint
     global cog_key
@@ -14,23 +18,45 @@ def main():
         cog_region = os.getenv('COG_SERVICE_REGION')
         translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
 
+        # Create client using endpoint and key
+        credential = TranslatorCredential(cog_key, cog_region)
+        client = TextTranslationClient(credential)
+
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
-        for file_name in os.listdir(reviews_folder):
-            # Read the file contents
-            print('\n-------------\n' + file_name)
-            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
-            print('\n' + text)
-
-            # Detect the language
-            language = GetLanguage(text)
-            print('Language:',language)
-
-            # Translate if not already English
-            if language != 'en':
-                translation = Translate(text, language)
-                print("\nTranslation:\n{}".format(translation))
-                
+        
+        # Detect the language
+        # language = GetLanguage(text)
+        # print('Language:',language)
+        languagesResponse = client.get_languages(scope="translation")
+        print("{} languages supported.".format(len(languagesResponse.translation)))
+        print("(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)")
+        print("Enter a target language code for translation (for example, 'en'):")
+        targetLanguage = "xx"
+        supportedLanguage = False
+        while supportedLanguage == False:
+            targetLanguage = input()
+            if  targetLanguage in languagesResponse.translation.keys():
+                supportedLanguage = True
+            else:
+                    print("{} is not a supported language.".format(targetLanguage))
+        # Translate if not already English
+        # if language != 'en':
+        #     translation = Translate(text, language)
+        #     print("\nTranslation:\n{}".format(translation))
+        # Translate text
+        inputText = ""
+        while inputText.lower() != "quit":
+            inputText = input("Enter text to translate ('quit' to exit):")
+            if inputText != "quit":
+                input_text_elements = [InputTextItem(text=inputText)]
+                translationResponse = client.translate(content=input_text_elements, to=[targetLanguage])
+                translation = translationResponse[0] if translationResponse else None
+                if translation:
+                    sourceLanguage = translation.detected_language
+                    for translated_text in translation.translations:
+                        print(f"'{inputText}' was translated from {sourceLanguage.language} to {translated_text.to} as '{translated_text.text}'.")
+        
     except Exception as ex:
         print(ex)
 
@@ -39,7 +65,19 @@ def GetLanguage(text):
     language = 'en'
 
     # Use the Azure AI Translator detect function
-
+    languagesResponse = client.get_languages(scope="translation")
+    languagesResponse = client.get_languages(scope="translation")
+    print("{} languages supported.".format(len(languagesResponse.translation)))
+    print("(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)")
+    print("Enter a target language code for translation (for example, 'en'):")
+    targetLanguage = "xx"
+    supportedLanguage = False
+    while supportedLanguage == False:
+        targetLanguage = input()
+        if  targetLanguage in languagesResponse.translation.keys():
+            supportedLanguage = True
+        else:
+            print("{} is not a supported language.".format(targetLanguage))
 
     # Return the language
     return language
@@ -48,7 +86,17 @@ def Translate(text, source_language):
     translation = ''
 
     # Use the Azure AI Translator translate function
-
+    inputText = ""
+    while inputText.lower() != "quit":
+        inputText = input("Enter text to translate ('quit' to exit):")
+        if inputText != "quit":
+            input_text_elements = [InputTextItem(text=inputText)]
+            translationResponse = client.translate(content=input_text_elements, to=[targetLanguage])
+            translation = translationResponse[0] if translationResponse else None
+            if translation:
+                sourceLanguage = translation.detected_language
+                for translated_text in translation.translations:
+                    print(f"'{inputText}' was translated from {sourceLanguage.language} to {translated_text.to} as '{translated_text.text}'.")
 
     # Return the translation
     return translation
